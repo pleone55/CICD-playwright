606c7cad8c13640c2ed80ec0b8e23eb778631132
Paul Leone <p-leone@hotmail.com>
2025-06-02 00:32:46 -0700

    run docker on github

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-    /usr/local/bin/node
+    /usr/bin/node
 else
     if [[ "$1" == "node" ]] || [[ "$1" == "npm" ]] || [[ "$1" == "npx" ]] || [[ "$1" == "sh" ]] || [[ "$1" == "bash" ]]; then
         $@
     else
-        /usr/local/bin/node "$@"
+        /usr/bin/node "$@"
     fi
 fi
